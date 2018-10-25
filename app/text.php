cc1d8f0f2ee629092ca330356bfe424c45aa2e83
Test <boyer.matthieu.85@gmail.com>
2018-10-25 14:59:40 +0200

    1st Commit : index.php stylesheet.css adding bootstrap form.php bootstrapForm.php text.php

---
+++ app/text.php
@@ -0,0 +1,17 @@
+<?php
+
+class Text{
+
+    const CURRENCY = " €";
+
+    public static function withZero($chiffre){
+        if($chiffre > 9){
+            return $chiffre;
+        }
+    return '0' . $chiffre;
+    }
+
+    public static function withEuro($chiffre){
+        return self::withZero($chiffre) . self::CURRENCY;
+    }
+}
